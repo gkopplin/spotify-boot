f66daa600c786c3538e1a0211ff9c6fae1e734fe
gkopplin <grant.kopplin@gmail.com>
2019-11-01 15:01:19 -0400

    Fix user list test

--- spotify-boot/src/test/java/com/example/spotifyboot/controller/UserControllerTest.java
+++ spotify-boot/src/test/java/com/example/spotifyboot/controller/UserControllerTest.java
@@ -3,48 +3,45 @@ package com.example.spotifyboot.controller;
 import com.example.spotifyboot.model.User;
 import com.example.spotifyboot.model.UserRole;
 import com.example.spotifyboot.service.UserService;
+import com.example.spotifyboot.util.JwtUtil;
 import org.junit.Before;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-//@RunWith(MockitoJUnitRunner.class)
 @WebMvcTest(UserController.class)
 public class UserControllerTest {
     private List<User> users = new ArrayList<User>();
 
-    @Mock
+    @MockBean
     UserService userService;
 
-    @InjectMocks
-    UserController userController;
+    @MockBean
+    private JwtUtil jwtUtil;
 
-    @InjectMocks
+    @Autowired
     private MockMvc mockMvc;
 
-    @InjectMocks
-    User user;
+    User user = new User();
 
     @Before
     public void init() {
-        mockMvc = MockMvcBuilders.standaloneSetup(userController).build();
-
         UserRole adminRole = new UserRole();
         adminRole.setName("ROLE_ADMIN");
 
@@ -54,19 +51,18 @@ public class UserControllerTest {
     }
 
     @Test
+    @WithMockUser(username = "batman", password = "bat", roles = {"ADMIN"})
     public void listUsers_Users_Success() throws Exception {
-//        users.add(user);
-//        RequestBuilder requestBuilder = MockMvcRequestBuilders
-//                .get("/user/list");
-//
-//        when(userService.listUsers()).thenReturn(users);
-//
-//        MvcResult result = mockMvc.perform(requestBuilder)
-//                .andExpect(status().isOk())
-//                .andExpect(content().json())
-//                .andReturn();
-//
-//        assertEquals(users, result.get)
-//        System.out.println(result.getResponse().getContentAsString());
+        users.add(user);
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .get("/user/list");
+
+        when(userService.listUsers()).thenReturn(users);
+
+        MvcResult result = mockMvc.perform(requestBuilder)
+                .andExpect(status().isOk())
+                .andExpect(content().json("[{\"id\":null,\"username\":\"George\",\"password\":\"Clooney\",\"songs\":null,\"userRole\":{\"id\":null,\"name\":\"ROLE_ADMIN\"}}]"))
+                .andReturn();
     }
 }
+
